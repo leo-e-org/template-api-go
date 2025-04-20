cb17ae2e3b3599334dcb7b4aa3873620d67d1c55
Leo <leo.ely@universo.univates.br>
2025-04-20 19:33:55 -0300

    Fixed Viper parsing

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,15 @@ package config
 import (
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"strings"
 	"template-api-go/internal/logger"
 )
 
 func LoadConfig() {
+	viper.AutomaticEnv()
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
-	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	err := viper.ReadInConfig()
 	if err != nil {
